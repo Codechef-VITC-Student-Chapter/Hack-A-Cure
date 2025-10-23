c78914656c861744886ef5c0e0974322eaaac18e
manmay2 <imanmay2@gmail.com>
2025-10-24 00:13:37 +0530

    API's build successfully.

---
+++ client/src/app/api/user/[userId]/route.ts
@@ -0,0 +1,21 @@
+//fetching the information for the specific userID from db
+
+import { NextResponse } from "next/server";
+import { connectDB } from "@/db/mongoose";
+import { User } from "@/models/user.model";
+
+
+export default async function GET( { params }: { params: { userId: string } }){
+    try{
+        await connectDB();
+        let {userId}= params;
+        let user=await User.findById(userId);
+        if(user.length==1){
+        return NextResponse.json({success:true,data:user});
+        } else{
+        return NextResponse.json({success:false,data:"User doesn't exists!!"});
+        }
+    } catch(err){
+        return NextResponse.json({success:false,data:String(err)},{status:500});
+    }
+}
---
+++ client/src/app/api/user/[userId]/submit/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/db/mongoose";
+import { User } from "@/models/user.model";
+
+export default async function POST(req: Request, { params }: { params: { userId: string } }) {
+    try {
+        await connectDB();
+        let { url } = await req.json();
+        let { userId } = params;
+        let updatedUser = await User.findByIdAndUpdate(userId, { $set: { url: url } });
+
+        if (!updatedUser) {
+            return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
+        }
+
+        return NextResponse.json({ success: true, user: updatedUser });
+
+    } catch (err) {
+        return NextResponse.json({ success: false, data: String(err) }, { status: 500 });
+    }
+}
